69de5d79745b4cac48b62afddd6ed9d2d35eee45
张学博 <a770919458@outlook.com>
2018-12-08 10:11:58 +0800

    添加了版本提示、开源协议

--- ZX.Web/Controllers/init/AdminController.cs
+++ ZX.Web/Controllers/init/AdminController.cs
@@ -13,11 +13,20 @@ namespace ZX.Web.Controllers.init
 {
     public class AdminController : Controller
     {
-        static string connStr = "Server=localhost;Integrated security=SSPI;database=master";
-        static string dataBaseName = "ZXOA";
         public ActionResult Index()
         {
             return View("initAdmin");
+        }
+
+        public ActionResult Agreement()
+        {
+            return View("agreement");
+        }
+
+        public ActionResult State()
+        {
+            ViewBag.version = ZXConfig.VERSION;
+            return View("state");
         }
 
         #region 动态校验用户输入的数据库连接信息是否正确
@@ -38,7 +47,7 @@ namespace ZX.Web.Controllers.init
                 dataBase = (DataBase)JsonConvert.DeserializeObject(json, dataBase.GetType());
 
                 string conStr = "data source=" + dataBase.Server + "; initial catalog=" + dataBase.DataBaseName + "; user id=" + dataBase.UserName + "; password=" + dataBase.Password;
-                SqlConnection m_SqlConnection = new SqlConnection(connStr);
+                SqlConnection m_SqlConnection = new SqlConnection(conStr);
                 m_SqlConnection.Open();
                 if (m_SqlConnection.State == ConnectionState.Open)
                 {
@@ -100,73 +109,5 @@ namespace ZX.Web.Controllers.init
         }
         #endregion
 
-        #region 还原数据库
-        /// <summary>
-        /// 还原数据库
-        /// </summary>
-        /// <param name="backfile"></param>
-        /// <returns></returns>
-        public bool RestoreDatabase(string backfile)
-        {
-            //标记数据库是否还原成功
-            bool flag = false;
-            ///杀死原来所有的数据库连接进程
-            SqlConnection conn = new SqlConnection(connStr);
-            conn.Open();
-            string sql = "SELECT spid FROM sysprocesses ,sysdatabases WHERE sysprocesses.dbid=sysdatabases.dbid AND sysdatabases.Name='" +
-                          dataBaseName + "'";
-            SqlCommand cmd1 = new SqlCommand(sql, conn);
-            SqlDataReader dr;
-            ArrayList list = new ArrayList();
-            try
-            {
-                dr = cmd1.ExecuteReader();
-                while (dr.Read())
-                {
-                    list.Add(dr.GetInt16(0));
-                }
-                dr.Close();
-            }
-            catch (SqlException ee)
-            {
-                Log4Helper.WriteError(ee.Message, ee);
-            }
-            finally
-            {
-                conn.Close();
-            }
-            //MessageBox.Show(list.Count.ToString());
-            for (int i = 0; i < list.Count; i++)
-            {
-                conn.Open();
-                cmd1 = new SqlCommand(string.Format("KILL {0}", list[i].ToString()), conn);
-                cmd1.ExecuteNonQuery();
-                conn.Close();
-                Log4Helper.WriteDebug("系统已经清除的数据库线程： " + list[i].ToString() + "\r\n正在还原数据库！");
-            }
-            //这里一定要是master数据库，而不能是要还原的数据库，因为这样便变成了有其它进程
-            //占用了数据库。
-            string database = dataBaseName;
-            string path = backfile;
-            string BACKUP = String.Format("RESTORE DATABASE {0} FROM DISK = '{1}'", database, path);
-            SqlConnection con = new SqlConnection(connStr);
-            SqlCommand cmd = new SqlCommand(BACKUP, con);
-            con.Open();
-            try
-            {
-                cmd.ExecuteNonQuery();
-                flag = true;
-            }
-            catch (SqlException ee)
-            {
-                Log4Helper.WriteError(ee.Message, ee);
-            }
-            finally
-            {
-                con.Close();
-            }
-            return flag;
-        }
-        #endregion
     }
 }
